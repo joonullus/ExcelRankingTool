db9dd40de120161659122438d40cd0470622c20a
Ceyhun Deniz Keleş <145282746+joonullus@users.noreply.github.com>
2024-09-04 03:55:15 +0300

    edited stuff to make the app publishable

--- src/application.py
+++ src/application.py
@@ -1,3 +1,6 @@
+# Excel RankingTool v1.0 by github.com/joonullus
+# README.md for more information
+
 import os
 import pandas as pd
 import ttkbootstrap as tk
@@ -35,7 +38,7 @@ class App(tk.Window):
             frame = F(container, self)
             self.frames[F] = frame
 
-        # Show the initial frame
+        # Shows the initial frame
         self.show_frame(StartPage)
 
         # Creates a Tkinter variable to monitor button actions
@@ -51,61 +54,63 @@ class App(tk.Window):
         signature = tk.Label(frame, text="github.com/joonullus", font=("Arial", 10))
         signature.place(relx=1.0, rely=1.0, anchor="se")
 
+    # This function does the entire ranking process by taking an excel file as the input and giving another excel file as the output.
+    # Excel file properties can be customized by changing the variables below.
     def ranking(self):
-        input_df = pd.read_excel('test.xlsx', sheet_name='Sheet1')
+        INPUT_FILE = 'input.xlsx'
+        INPUT_FILE_SHEET_NAME = 'Sheet1'
+        INPUT_FILE_COLUMN_NAME = 'Elements'
+        OUTPUT_FILE = 'output.xlsx'
+
+        input_df = pd.read_excel(INPUT_FILE, sheet_name=INPUT_FILE_SHEET_NAME)
 
         # Checks if the output file already exists.
         # If it does, the process continues from the already existing file.
         # Otherwise, a new output file is created.
         # This allows the user to keep the progress if the app is closed before the process is over.
-        if os.path.isfile('output.xlsx'):
-            output_df = pd.read_excel('output.xlsx', sheet_name='Sheet1')
+        if os.path.isfile(OUTPUT_FILE):
+            output_df = pd.read_excel(OUTPUT_FILE, sheet_name='Sheet1')
         else:
             output_df = pd.DataFrame({
-                'Openings': [str(input_df.loc[0, 'Anime Name']) + ' Opening ' + str(input_df.loc[0, 'Opening Number'])]
+                INPUT_FILE_COLUMN_NAME : [str(input_df.loc[0, INPUT_FILE_COLUMN_NAME])]
             })
-            output_df.to_excel('output.xlsx', index=False)
+            output_df.to_excel(OUTPUT_FILE, index=False)
         
         # Gets the total number of rows in the input and the output.
         no_of_total_rows_input = input_df.shape[0]
         no_of_rows_output = output_df.shape[0]
 
         # Binary insertion sort algorithm to reduce the number of user comparisons.
-        # The progress can be tracked using the number of rows in the output file.
+        # Also allows the progress to be tracked by looking at the number of rows in the output file.
         for i in range(no_of_rows_output, no_of_total_rows_input):
-            choice2 = str(input_df.loc[i, 'Anime Name']) + ' Opening ' + str(input_df.loc[i, 'Opening Number'])
+            choice2 = str(input_df.loc[i, INPUT_FILE_COLUMN_NAME])
 
             low = 0
             high = output_df.shape[0] - 1
             position = -1
-            get_position = False
 
             while low <= high:
                 mid = (high + low) // 2
-                choice1 = str(output_df.loc[mid, 'Openings'])
+                choice1 = str(output_df.loc[mid, INPUT_FILE_COLUMN_NAME])
 
                 self.frames[ComparisonPage].create_buttons(choice1, choice2)
                 self.wait_variable(self.choice)
-
-                if low == high:
-                    get_position = True
                 
                 if self.choice.get() == choice1:
                     low = mid + 1
-                    if get_position:
-                        position = low
+                    position = low
                 else:
                     high = mid - 1
-                    if get_position:
-                        position = mid
+                    position = mid
 
-            new_row = {'Openings': choice2}
+            new_row = {INPUT_FILE_COLUMN_NAME: choice2}
             new_row_df = pd.DataFrame([new_row], index=[position])
             output_df = pd.concat([output_df.iloc[:position], new_row_df, output_df.iloc[position:]]).reset_index(drop=True)
-            output_df.to_excel('output.xlsx', index=False)
+            output_df.to_excel(OUTPUT_FILE, index=False)
         
         self.show_frame(EndPage)
-            
+
+# Class for the StartPage which is the title screen.
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
@@ -122,6 +127,7 @@ class StartPage(tk.Frame):
         self.controller.show_frame(ComparisonPage)
         self.controller.ranking()
 
+# Class for the ComparionPage where the entire ranking process is handled. 
 class ComparisonPage(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
@@ -142,7 +148,8 @@ class ComparisonPage(tk.Frame):
         self.controller.choice.set(picked_choice)
         self.button1.destroy()
         self.button2.destroy()
-    
+
+# Class for the EndPage which shows up once the process is over. 
 class EndPage(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
